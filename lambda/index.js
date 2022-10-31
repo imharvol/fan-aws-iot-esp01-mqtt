41573a7c5be090ab92a76b138ee8222df72d6601
imharvol <imharvol@gmail.com>
2022-10-31 18:17:49 +0100

    Add lambda function code

---
+++ lambda/index.js
@@ -0,0 +1,43 @@
+const { IoTDataPlaneClient, UpdateThingShadowCommand } = require('@aws-sdk/client-iot-data-plane')
+
+exports.handler = async (event) => {
+  console.log(event)
+
+  let fanOn
+  if (event?.queryStringParameters?.fanOn === 'true') {
+    fanOn = true
+  } else if (event?.queryStringParameters?.fanOn === 'false') {
+    fanOn = false
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        msg: `"fanOn" attribute should be either "true" or "false"`
+      })
+    }
+  }
+
+  const client = new IoTDataPlaneClient({})
+
+  const updateCommand = new UpdateThingShadowCommand({
+    thingName: 'ventilador',
+    payload: Buffer.from(
+      JSON.stringify({
+        state: {
+          reported: {
+            on: fanOn
+          }
+        }
+      })
+    )
+  })
+  const updateResponse = await client.send(updateCommand)
+  console.log(updateResponse)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      msg: 'ok'
+    })
+  }
+}
